87d782c330dd893d7de81e8a5862c89ac120f0a2
Nebulo9 <matdessauv@gmail.com>
2023-12-09 14:01:11 +0100

    Put instructions into a function

--- notif.py
+++ notif.py
@@ -1,10 +1,16 @@
 # A EXECUTER SUR WINDOWS UNIQUEMENT
 from win10toast import ToastNotifier
+import time
 
-TITLE = "Notification"
-MESSAGE = "Hello World!"
-THREADED = True
+def send_notification(title, message,duration=3,icon_path=None,threaded=False,cooldown=None):
+    toaster = ToastNotifier()
+    toaster.show_toast(title, message,icon_path=icon_path,duration=duration,threaded=threaded)
+    if cooldown:
+        time.sleep(duration + cooldown)
+    else:
+        while toaster.notification_active():
+            time.sleep(0.1)
 
 if __name__ == "__main__":
-    toaster = ToastNotifier()
-    toaster.show_toast(TITLE, MESSAGE, threaded=THREADED)
+    send_notification("test","test")
+    send_notification("test2","test2")
